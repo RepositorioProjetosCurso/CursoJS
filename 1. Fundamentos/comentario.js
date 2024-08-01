0306c52b74df4ef21fa2e55c5ac8c68f02a5f0a6
RepositorioCursos <repositoriocursosgit@gmail.com>
2024-08-01 16:59:31 -0300

    Criando arquivo de exemplo de comentários

---
+++ 1. Fundamentos/comentario.js
@@ -0,0 +1,14 @@
+// Comentário de uma única linha
+console.log("Linha 01");
+
+/*
+Comentário de 
+multiplas linhas
+*/
+console.log("Linha 02");
+
+/*
+* Comentário de
+* Multiplas linhas
+*/
+console.log("Linha 03");
